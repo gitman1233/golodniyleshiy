ba9369c3b364b7e61cd5243e13ef7b0c62b347fa
gitman1233 <67467601+gitman1233@users.noreply.github.com>
2025-11-27 17:34:47 +0500

    Update golodniy_converter.py

--- golodniy_converter.py
+++ golodniy_converter.py
@@ -86,6 +86,7 @@ def webhook():
         f"Телефон: {data.get('Phone', '') or data.get('ma_phone', '')}\n"
         f"Email: {data.get('Email', '') or data.get('ma_email', '')}\n"
         f"ФИО: {fio}\n"
+        f"Комментарий к заказу: {data.get('delivery_comment', '')}\n"
     )
 
     payload = {
